4bfb6bd6bc73eef138d07befecc4c47372efef5d
Thefcraft <76678136+thefcraft@users.noreply.github.com>
2025-01-16 20:20:19 +0530

    hero rotating and sponsor and fix footer

--- pages/index.js
+++ pages/index.js
@@ -20,7 +20,7 @@ import styles from '../styles/homepage.module.css'
 // import AwesomeSlider from 'react-awesome-slider'
 // import 'react-awesome-slider/dist/styles.css'
 // import { motion } from 'framer-motion'
-
+import HeroSection from '../components/Hero/Hero'
 
 const cn = (...classes) => {
     return classes.filter(Boolean).join(' ');
@@ -146,25 +146,31 @@ const index = () => {
             body: "Solo - Classical Music Competition"
         }
     ];
-    const pseudoSponsorImage = [
-        "/pics/sponsor/pizza_hut.jpg",
-        "/pics/sponsor/sbi.jpg",
-        "/pics/sponsor/zomato.png",
-        "/pics/sponsor/pizza_hut.jpg",
-        "/pics/sponsor/sbi.jpg",
-        "/pics/sponsor/zomato.png",
-        "/pics/sponsor/pizza_hut.jpg",
-        "/pics/sponsor/sbi.jpg",
-        "/pics/sponsor/zomato.png",
-        "/pics/sponsor/pizza_hut.jpg",
-        "/pics/sponsor/sbi.jpg",
-        "/pics/sponsor/zomato.png",
-        "/pics/sponsor/pizza_hut.jpg",
-        "/pics/sponsor/sbi.jpg",
-        "/pics/sponsor/zomato.png",
-        "/pics/sponsor/pizza_hut.jpg",
-        "/pics/sponsor/sbi.jpg",
-        "/pics/sponsor/zomato.png"
+    const sponsorImages = [
+        'https://drive.google.com/uc?export=view&id=1sk_dXvHZCLN5QGH8x5ae4vjunza7kdwo', // 'Allen Cooper'
+        'https://drive.google.com/uc?export=view&id=1sO3UC-XMYPAggeQ_P3loZCSxjbXKiTzk', // 'Bihar Tourism'
+        'https://drive.google.com/uc?export=view&id=1NzTZh6D-THmqConUzrZ2tvC3PaJU3Ets', // 'Biryani'
+        'https://drive.google.com/uc?export=view&id=10JmGTjBV_wliW6EoE4ozwnErEHNWZlGe', // 'Boult'
+        'https://drive.google.com/uc?export=view&id=1sD17yO4Zwm449d9ilTh9cPRoTeUd4tpy', // 'Bihar Rajya Pul Nirman Nigam'
+        'https://drive.google.com/uc?export=view&id=1yCcmexPef2xI3lQXo7wJAzliDEyUIxh-', // 'Bihar State Aids Control Society'
+        'https://drive.google.com/uc?export=view&id=1QZLHYPSJsMLQUKssih7HPqU5DwMGkcnc', // 'Bihar State Electronics Development Corporation'
+        'https://drive.google.com/uc?export=view&id=1UBiVYAM7HGv-tjMiyTofA7_ZCWS959MG', // 'Bihar Council on Science and Technology'
+        'https://drive.google.com/uc?export=view&id=1gg9J_on8QTdBs64u7TVwpwkc45ydw4Eq', // 'Department of Information Technology'
+        'https://drive.google.com/uc?export=view&id=1FOtkwzHkA74tK7uMzHYaxSbz8W-N40EN', // 'Department of Industries'
+        'https://drive.google.com/uc?export=view&id=1EKapSE6Q9xbDmycY0XecwCEHKjxUqZzO', // 'Dominos'
+        'https://drive.google.com/uc?export=view&id=1_LI78ee0KbgufhKeFSNQJSK7DzqG6T6q', // 'Fueling'
+        'https://drive.google.com/uc?export=view&id=1NXlcqgtvPwuaGzr0KxNWWaIGvNgUWDax', // 'Hydration'
+        'https://drive.google.com/uc?export=view&id=1Ge6eJMxha0lNxzF8AXW-aT8fP11FXJHA', // 'Kala Akchar'
+        'https://drive.google.com/uc?export=view&id=1lvnREbJ84WauvAhwBCrtO7P11HO-HUz9', // 'NHAI'
+        'https://drive.google.com/uc?export=view&id=13oaVnvbhSXcAZTTajdcwXVJ-4YjGtRBu', // 'NTPC'
+        'https://drive.google.com/uc?export=view&id=12n_N2f4FyomvBlLOELPnGhLjgPdzCfkb', // 'Red FM'
+        'https://drive.google.com/uc?export=view&id=13M6yySVuABxMP7pus6Hy2HVbianRpSnA', // 'Road Chef'
+        'https://drive.google.com/uc?export=view&id=1MLfxr5ipb_m3VUkNCiAB3sc00hxz5fi-', // 'State Bank of India'
+        'https://drive.google.com/uc?export=view&id=1Vbu1tCEMNPzoeOqpydOcOSxHlPzVz3up', // 'Startup Bihar'
+        'https://drive.google.com/uc?export=view&id=1AA3qGrGqqTbmfo2DAWmkAWXs8KWTDC07', // 'The Community Events'
+        'https://drive.google.com/uc?export=view&id=1Ub5Ntbu30Kp-1dpYSeB0M_QtbjGsiVpI', // 'Times of India'
+        'https://drive.google.com/uc?export=view&id=1mX_WeCIywRV838QPn8AywiEWTSXSzMbM', // 'Waffcha'
+        'https://drive.google.com/uc?export=view&id=183hiDaFhULaFvHURLFMCWBPmT7RjMRWI', // 'Wat A Burger'
     ];
     const pseudoMomentImage = [
         "/pics/moments/dj.png",
@@ -192,7 +198,7 @@ const index = () => {
         </Head>
         <div className={styles.bg}>
             {/* HERO */}
-            <section className={styles.hero}>
+            <HeroSection className={styles.hero}>
                 <div className={styles.hero_main} />
                 <div className={styles.hero_text}>
                     <Image src={'/pics/hero_image-export.svg'} width={1047} height={589}/>
@@ -201,7 +207,7 @@ const index = () => {
                 <div className={styles.hero_button}>
                     <button className={cn(styles.sexy_button, styles.sexy_button_small)}>REGISTER</button>
                 </div>  
-            </section>
+            </HeroSection>
 
             {/* Events */}
             <section className={styles.events}>
@@ -356,15 +362,15 @@ const index = () => {
             </section>
 
             {/* Sponsors */}
-            {/* <section className={styles.sponsors}>
+            <section className={styles.sponsors}>
                 <div className={styles.sponsors_title}>
                     <h2>Our Proud Sponsors</h2>
                     <h3>Strengthening the Vision Together</h3>
                 </div>
                 <div className={styles.sponsors_images_slider}>
-                    <SponsorsSlider images={pseudoSponsorImage}/>
+                    <SponsorsSlider images={sponsorImages}/>
                 </div>
-            </section> */}
+            </section>
         </div>
     </>)
 }
