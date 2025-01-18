0feb0fea6cbe6aa2b42fd6c6d7b04d211bef0812
Harshvardhan <harshchauhan97194@gmail.com>
2025-01-18 17:25:00 +0530

    events updated

--- pages/events.js
+++ pages/events.js
@@ -7,9 +7,10 @@ import Head from 'next/head'
 import { useEffect, useState } from 'react'
 import Card from '../components/EventItem/index.js'
 import { useNavigate } from 'react-router-dom';
-import Modal from '../components/EventItem/Modal.js'
+// import Modal from '../components/EventItem/Modal.js'
 import EventItem from '../components/EventItem'
 import eventsDetails from '../public/events/events_data.json' assert { type: 'json' };
+import Modal from '../components/BigModal/index.js'
 
 
 const montserrat = Montserrat({
@@ -32,10 +33,10 @@ const eventsinfo = {
     ],
 }
 
-const workshopcardarr=[{
-    "Event Name":"techgyan x Anwesha",
-    "poster":'/events/workshopPoster.jpeg',
-    "Event":"2 days of workshop",
+const workshopcardarr = [{
+    "Event Name": "techgyan x Anwesha",
+    "poster": '/events/workshopPoster.jpeg',
+    "Event": "2 days of workshop",
 }]
 
 const josefinSans = Josefin_Sans({
@@ -43,10 +44,10 @@ const josefinSans = Josefin_Sans({
     subsets: ['latin'],
 })
 
-const SponsorsSlider = ({images, animation_duration=-1}) => {
+const SponsorsSlider = ({ images, animation_duration = -1 }) => {
     const width = 127.381; // IF YOU CHANGE THIS THEN CHANGE IT INSIDE autoScrollSponseAnimation ALSO
     const heigth = 127.381;
-    const duration = animation_duration<=0?Math.floor(10*(images.length / 7)):animation_duration;
+    const duration = animation_duration <= 0 ? Math.floor(10 * (images.length / 7)) : animation_duration;
 
     const gap = 16;
     return <div style={{
@@ -57,17 +58,17 @@ const SponsorsSlider = ({images, animation_duration=-1}) => {
         height: heigth
     }}>
         {
-            images.map((src, index) => 
+            images.map((src, index) =>
                 <div key={index} className={styles.autoScrollSponseAnimation} style={{
                     width: "100%",
                     position: "absolute",
                     left: "100%",
                     // zIndex: 8,
-                    animationDelay: `${(duration/images.length)*index}s`,
+                    animationDelay: `${(duration / images.length) * index}s`,
                     animationDuration: `${duration}s`,
                     '--width': width
                 }}>
-                    <Image src={src} width={width} height={heigth}/>
+                    <Image src={src} width={width} height={heigth} />
                 </div>
             )
         }
@@ -76,7 +77,7 @@ const SponsorsSlider = ({images, animation_duration=-1}) => {
 
 const Events = () => {
     const [fadeOut, setFadeOut] = useState(false)
- 
+
     // Handling fade-out effect on scroll   
     useEffect(() => {
         const handleScroll = () => {
@@ -96,52 +97,33 @@ const Events = () => {
 
     // add event ids which won't be shown on website
     let exceludedEvents = [
-        'EVTcf525',
-        'EVTcac95',
-        'EVT66e40',
-        'EVT68cb3',
-        'EVT49870',
-        'EVTe96c6',
-        'EVT8e600',
-        'EVT7a8a7',
-        'EVT691bc',
-        'EVTba113',
-        'EVT2842f',
-        'EVT63763',
-        'EVTcb689',
-        'EVTa1ae2',
-        'EVT20570',
-        'EVT9dda6',
-        'EVTbdb92',
-        'EVT12910',
-        'EVT8d60d',
-        'EVT291e3',
-        'EVT9fcc8',
-        'EVTc90d3',
-        'EVTbdbc6',
-        'EVT49b71',
-        'EVT3264e',
     ]
 
-    const pseudoSponsorImage = [
-        "/pics/sponsor/pizza_hut.jpg",
-        "/pics/sponsor/sbi.jpg",
-        "/pics/sponsor/zomato.png",
-        "/pics/sponsor/pizza_hut.jpg",
-        "/pics/sponsor/sbi.jpg",
-        "/pics/sponsor/zomato.png",
-        "/pics/sponsor/pizza_hut.jpg",
-        "/pics/sponsor/sbi.jpg",
-        "/pics/sponsor/zomato.png",
-        "/pics/sponsor/pizza_hut.jpg",
-        "/pics/sponsor/sbi.jpg",
-        "/pics/sponsor/zomato.png",
-        "/pics/sponsor/pizza_hut.jpg",
-        "/pics/sponsor/sbi.jpg",
-        "/pics/sponsor/zomato.png",
-        "/pics/sponsor/pizza_hut.jpg",
-        "/pics/sponsor/sbi.jpg",
-        "/pics/sponsor/zomato.png"
+    const sponsorImages = [
+        'https://drive.google.com/uc?export=view&id=1sk_dXvHZCLN5QGH8x5ae4vjunza7kdwo', // 'Allen Cooper'
+        'https://drive.google.com/uc?export=view&id=1sO3UC-XMYPAggeQ_P3loZCSxjbXKiTzk', // 'Bihar Tourism'
+        'https://drive.google.com/uc?export=view&id=1NzTZh6D-THmqConUzrZ2tvC3PaJU3Ets', // 'Biryani'
+        'https://drive.google.com/uc?export=view&id=10JmGTjBV_wliW6EoE4ozwnErEHNWZlGe', // 'Boult'
+        'https://drive.google.com/uc?export=view&id=1sD17yO4Zwm449d9ilTh9cPRoTeUd4tpy', // 'Bihar Rajya Pul Nirman Nigam'
+        'https://drive.google.com/uc?export=view&id=1yCcmexPef2xI3lQXo7wJAzliDEyUIxh-', // 'Bihar State Aids Control Society'
+        'https://drive.google.com/uc?export=view&id=1QZLHYPSJsMLQUKssih7HPqU5DwMGkcnc', // 'Bihar State Electronics Development Corporation'
+        'https://drive.google.com/uc?export=view&id=1UBiVYAM7HGv-tjMiyTofA7_ZCWS959MG', // 'Bihar Council on Science and Technology'
+        'https://drive.google.com/uc?export=view&id=1gg9J_on8QTdBs64u7TVwpwkc45ydw4Eq', // 'Department of Information Technology'
+        'https://drive.google.com/uc?export=view&id=1FOtkwzHkA74tK7uMzHYaxSbz8W-N40EN', // 'Department of Industries'
+        'https://drive.google.com/uc?export=view&id=1EKapSE6Q9xbDmycY0XecwCEHKjxUqZzO', // 'Dominos'
+        'https://drive.google.com/uc?export=view&id=1_LI78ee0KbgufhKeFSNQJSK7DzqG6T6q', // 'Fueling'
+        'https://drive.google.com/uc?export=view&id=1NXlcqgtvPwuaGzr0KxNWWaIGvNgUWDax', // 'Hydration'
+        'https://drive.google.com/uc?export=view&id=1Ge6eJMxha0lNxzF8AXW-aT8fP11FXJHA', // 'Kala Akchar'
+        'https://drive.google.com/uc?export=view&id=1lvnREbJ84WauvAhwBCrtO7P11HO-HUz9', // 'NHAI'
+        'https://drive.google.com/uc?export=view&id=13oaVnvbhSXcAZTTajdcwXVJ-4YjGtRBu', // 'NTPC'
+        'https://drive.google.com/uc?export=view&id=12n_N2f4FyomvBlLOELPnGhLjgPdzCfkb', // 'Red FM'
+        'https://drive.google.com/uc?export=view&id=13M6yySVuABxMP7pus6Hy2HVbianRpSnA', // 'Road Chef'
+        'https://drive.google.com/uc?export=view&id=1MLfxr5ipb_m3VUkNCiAB3sc00hxz5fi-', // 'State Bank of India'
+        'https://drive.google.com/uc?export=view&id=1Vbu1tCEMNPzoeOqpydOcOSxHlPzVz3up', // 'Startup Bihar'
+        'https://drive.google.com/uc?export=view&id=1AA3qGrGqqTbmfo2DAWmkAWXs8KWTDC07', // 'The Community Events'
+        'https://drive.google.com/uc?export=view&id=1Ub5Ntbu30Kp-1dpYSeB0M_QtbjGsiVpI', // 'Times of India'
+        'https://drive.google.com/uc?export=view&id=1mX_WeCIywRV838QPn8AywiEWTSXSzMbM', // 'Waffcha'
+        'https://drive.google.com/uc?export=view&id=183hiDaFhULaFvHURLFMCWBPmT7RjMRWI', // 'Wat A Burger'
     ];
 
     useEffect(() => {
@@ -178,7 +160,7 @@ const Events = () => {
         setIsModalOpen(true)
     }
 
-    
+
     const closeModal = () => {
         setIsModalOpen(false)
         setSelectedEvent(null)
@@ -205,13 +187,13 @@ const Events = () => {
             <div className={styles.container}>
                 <div className={styles.titleBox}>
                     <div className={styles.centergroup}>
-                       <img src="/events/center_material.svg" alt="" />
+                        <img src="/events/center_material.svg" alt="" />
                     </div>
                     <div className={styles.titletext}>EXPLORE THE EVENTS</div>
                 </div>
                 <div className={`${styles.searchContainer}`}>   {/*  ${fadeOut ? styles.fadeOut : ''}  */}
                     <div className={styles.leftgroup}>
-                      <img src="/events/left_material.svg" alt="" />
+                        <img src="/events/left_material.svg" alt="" />
                     </div>
                     <div
                         className={`${styles.searchbox} `}
@@ -224,27 +206,28 @@ const Events = () => {
                         <img src="/events/search_icon.svg" alt="" />
                     </div>
                     <div className={styles.rightgroup}>
-                       <img src="/events/right_material.svg" alt="" />
+                        <img src="/events/right_material.svg" alt="" />
                     </div>
                 </div>
 
                 <div className={styles.cardContainer}>
-                    {eventsDetails.map((item, idx) => (
+                    {events.map((item, idx) => (
                         <Card
                             onClick={() => openModal(item)}
                             key={idx}
                             event={item}
+                            closeHandler={closeModal}
                         />
                     ))}
                 </div>
                 {isModalOpen && (
-                <Modal
-                    isOpen={isModalOpen}
-                    onClose={closeModal}
-                    event={selectedEvent}
-                />
-            )}
-           
+                    <Modal
+                        title={selectedEvent.name.split('#')[0]}
+                        body={selectedEvent}
+                        closeHandler={closeModal}
+                    />
+                )}
+
             </div>
             {/* Sponsors */}
             <section className={styles.sponsors}>
@@ -253,7 +236,7 @@ const Events = () => {
                     <h3>Strengthening the Vision Together</h3>
                 </div>
                 <div className={styles.sponsors_images_slider}>
-                    <SponsorsSlider images={pseudoSponsorImage}/>
+                    <SponsorsSlider images={sponsorImages} />
                 </div>
             </section>
         </div>
