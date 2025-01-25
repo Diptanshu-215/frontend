3f490de78544cc5a82e5ae83b06c2bb841d08e45
Swam244 <123237756+Swam244@users.noreply.github.com>
2025-01-25 22:58:56 +0530

    Update ourteam.js

--- pages/ourteam.js
+++ pages/ourteam.js
@@ -196,6 +196,12 @@ export default function ourteam() {
         ],
         rsp: [
             {
+                name: 'Sakshi Srivastava', image: '/team/sakshi_eee.jpg',
+                instagram: 'https://www.instagram.com/isakshi_shrivastava?utm_source=qr&igsh=MWY1OXp1ZHljbnRwMA==',
+                linkedin: 'https://www.linkedin.com/in/sakshi-shrivastava-05649925a?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app',
+                email: '2201ee81_sakshi@iitp.ac.in'
+            }
+            {
                 name: 'Harsh Mittal', image: '/team/harshm.jpg',
                 instagram: 'https://www.instagram.com/harshmittal2309?igsh=MWcxazQ5dWhjODVxOQ==',
                 linkedin: 'https://www.linkedin.com/in/harsh-mittal-7490a7254?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app',
