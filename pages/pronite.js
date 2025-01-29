c165d6c89f001e609230e6a6dff8ef7295aacfda
Harshvardhan <harshchauhan97194@gmail.com>
2025-01-30 02:21:52 +0530

    minor changes

--- pages/pronite.js
+++ pages/pronite.js
@@ -9,49 +9,10 @@ const Pronite = () => {
     const router = useRouter()
     useEffect(() => {
         const handleRegistration = () => {
-            router.push('/registration')
+            router.push('/userLogin')
         }
         handleRegistration()
     }, [])
-    const call = () => {
-        proniteRegistration('FESTPASS',
-            userData.state.user.email_id,
-            userData.state.user.phone_number,
-            userData.state.user.anwesha_id,
-            userData.state.user.user_type,
-            router)
-    }
-
-    async function event() {
-        var myHeaders = new Headers()
-        myHeaders.append('Content-Type', 'application/json')
-
-        var raw = JSON.stringify({
-            anwesha_id: userData.state.user.anwesha_id,
-        })
-        var requestOptions = {
-            method: 'POST',
-            headers: myHeaders,
-            body: raw,
-            redirect: 'follow',
-            credentials: 'include',
-        }
-
-        const data = await fetch(`${host}/festpasses/get`, requestOptions)
-            .then((response) => response.json())
-            .catch((error) => {
-                console.error(error)
-            })
-        console.log(data)
-    }
-
-
-    return (
-        <div style={{ height: '300px', width: '200px', color: 'white', marginTop: '200px' }}>
-            <button onClick={call}>submit here</button>
-            <button onClick={event}>fetch pass</button>
-        </div>
-    )
 }
 
 export default Pronite
