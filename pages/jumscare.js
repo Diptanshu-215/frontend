40c99d652f07eb052fdbfcf3b99eb0d97818d396
“[ajaykumar <[nabalsaini231@gmail.com]>
2025-02-01 23:28:11 +0530

    error solve may be

--- pages/jumscare.js
+++ pages/jumscare.js
@@ -1,27 +1,34 @@
-import React, { useState, useEffect } from "react";
-import styles from "./jumscare.module.css";
-
+import React, { useState, useEffect } from 'react'
+import styles from './jumscare.module.css'
+import Image from 'next/image'
 
 const VideoPlayer = () => {
-  const [isMobile, setIsMobile] = useState(window.innerWidth < 700);
+    const [isMobile, setIsMobile] = useState(window.innerWidth < 700)
 
-  useEffect(() => {
-    const handleResize = () => {
-      setIsMobile(window.innerWidth < 700);
-    };
+    useEffect(() => {
+        const handleResize = () => {
+            setIsMobile(window.innerWidth < 700)
+        }
 
-    window.addEventListener("resize", handleResize);
-    return () => window.removeEventListener("resize", handleResize);
-  }, []);
+        window.addEventListener('resize', handleResize)
+        return () => window.removeEventListener('resize', handleResize)
+    }, [])
 
-  return (
-    <div className={styles.jumscare_div}>
-      {!isMobile && (
-        <img className={styles.img} src="/pics/jump.gif" alt="Scary GIF" />
-      )}
-      {/* Audio element removed to prevent playback */}
-    </div>
-  );
-};
+    return (
+        <div className={styles.jumscare_div}>
+            {!isMobile && (
+                // <img className={styles.img} src="/pics/jump.gif" alt="Scary GIF" />
+                <Image
+                    className={styles.img}
+                    src="/pics/jump.gif"
+                    alt="Scary GIF"
+                    width={200}
+                    height={100}
+                />
+            )}
+            {/* Audio element removed to prevent playback */}
+        </div>
+    )
+}
 
-export default VideoPlayer;
+export default VideoPlayer
