98431d7ce114287d075339ed75ca137d9baca508
Harshvardhan <harshchauhan97194@gmail.com>
2025-02-02 15:24:32 +0530

    some minor changes

--- pages/userRegister.js
+++ pages/userRegister.js
@@ -12,6 +12,12 @@ export default function userRegister() {
             </Head>
 
             <div className={styles.container} loading="lazy">
+                <div style={{ padding: '3px 6px', backgroundColor: 'black', position: 'absolute', top: '82px', left: '50%', transform: 'translateX(-50%)', borderRadius: '4px' }}>
+                    <p style={{ color: 'white' }}>
+                        Non IITP Students register by their personal email
+                    </p>
+                </div>
+
                 <UserRegisterForm />
                 {/* <img
                     className={styles.island}
